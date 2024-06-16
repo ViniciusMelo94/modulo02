3e937f48820abdee8072323939de03bdec5e6056
Vinicius Valcedir de Melo <vinicius_v_melo@estudante.sesisenai.org.br>
2024-06-16 19:01:39 -0300

    exercicio semana 03

---
+++ exercicios/semana3/EX05/index.js
@@ -0,0 +1,21 @@
+const botao_ADC = document.querySelector("#buttonADC")
+const text_ADC = document.querySelector("#adc_text")
+const criar_LISTA = document.querySelector(".list_type")
+const minha_LISTA = []
+
+botao_ADC.addEventListener('click', (event) => {
+    const valor = text_ADC.value
+    minha_LISTA.push(valor)
+
+    const listaElementos = minha_LISTA.map(item => `<li><input type="checkbox" class="check_ITEM"> 
+ ${item}</li>`)
+    criar_LISTA.innerHTML = listaElementos.join("")
+
+    const adc_SUBLIME = document.querySelector("li")
+    adc_SUBLIME.addEventListener('change', () => {
+        adc_SUBLIME.classList.toggle('check_ITEMUNDER')
+    })
+
+
+
+})
